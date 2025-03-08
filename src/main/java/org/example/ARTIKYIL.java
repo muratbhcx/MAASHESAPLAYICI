5fa68f67c6870be8872c0588bf2881857213cdb4
Murat <muratbehucbuga@gmail.com>
2025-03-08 14:41:50 +0300

    ARTIKYIL PROJE

---
+++ src/main/java/org/example/ARTIKYIL.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ARTIKYIL {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Yıl Giriniz : ");
+        int year = scanner.nextInt();
+        if (year % 100 == 0 ) {
+            if (year % 400 == 0) {
+                System.out.println(year + " bir artık yıldır !");
+            } else {
+                System.out.println(year + " bir artık yıl değildir !");
+            }
+            }else if (year % 4 == 0) {
+            System.out.println(year + " bir artık yıldır !");
+        }else {
+            System.out.println(year + " bir artık yıl değildir !");
+        }
+            }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
