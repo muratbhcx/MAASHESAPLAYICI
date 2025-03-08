79fa4aa33d7299f871800d02b99a0114c6b35c55
Murat <muratbehucbuga@gmail.com>
2025-03-08 14:45:03 +0300

    VUCUT KITLE PROJE

---
+++ src/main/java/org/example/BOYKILO.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class BOYKILO {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinden) giriniz: ");
+        double size = scanner.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz:");
+        double weight = scanner.nextDouble();
+
+        double index = weight / (size * size);
+        System.out.println("Vücut Kitle İndeksiniz: " + index);
+
+
+
+    }
+}
