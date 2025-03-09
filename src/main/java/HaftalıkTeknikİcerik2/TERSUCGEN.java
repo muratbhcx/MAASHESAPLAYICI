99b7fafe105ef5921039f46ad8b5e1b67f211433
Murat <muratbehucbuga@gmail.com>
2025-03-09 19:13:47 +0300

    TERS UCGEN PROJE

---
+++ src/main/java/HaftalıkTeknikİcerik2/TERSUCGEN.java
@@ -0,0 +1,17 @@
+package HaftalıkTeknikİcerik2;
+
+import java.util.Scanner;
+
+public class TERSUCGEN {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir sayı giriniz : ");
+        int number = scanner.nextInt();
+        for (int i = 1; i <= number; i++) {
+            for (int j = number; i <= j; --j) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
