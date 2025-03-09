fefe9f3a7bb11f5aa6c09bc1768a22ac7b36f5e5
Murat <muratbehucbuga@gmail.com>
2025-03-09 22:52:28 +0300

    TIP DONUSUMU PROJE

---
+++ src/main/java/HaftalıkTeknikİcerik3/TIPDONUSUMU.java
@@ -0,0 +1,17 @@
+package HaftalıkTeknikİcerik3;
+
+import java.util.Scanner;
+
+public class TIPDONUSUMU {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir tam sayı giriniz : ");
+        int integer = scanner.nextInt();
+        System.out.println("Bir ondalıklı sayı giriniz : ");
+        double decimal = scanner.nextDouble();
+        double decimalnumber = integer;
+        System.out.println("Tam sayının ondalıklı sayıya dönüşümü : " + decimalnumber);
+        int integer1 = (int) decimal;
+        System.out.println("Ondalıklı sayının tam sayıya dönüşümü : " +integer1);
+    }
+}
